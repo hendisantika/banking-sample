8d49f72d28b71163a4ca9ae7c8f86b62b55ac3a6
hendisantika <hendisantika@yahoo.co.id>
2025-12-30 07:43:07 +0700

    feat: Init Project🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/bankingsample/BankingSampleApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.bankingsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingSampleApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(BankingSampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/my/hendisantika/bankingsample/BankingSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.bankingsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
