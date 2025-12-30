b5c1de0019487cdb459eb62ebb61b0f5406302b2
hendisantika <hendisantika@yahoo.co.id>
2025-12-30 07:46:21 +0700

    feat: Add ACTION Enum🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/bankingsample/constants/ACTION.java
@@ -0,0 +1,17 @@
+package id.my.hendisantika.bankingsample.constants;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : banking-sample
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 30/12/25
+ * Time: 07.46
+ * To change this template use File | Settings | File Templates.
+ */
+public enum ACTION {
+    DEPOSIT,
+    WITHDRAW
+}
