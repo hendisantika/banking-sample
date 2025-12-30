561b2b038ad82a59daf984517eff83c8a1558fc9
hendisantika <hendisantika@yahoo.co.id>
2025-12-30 07:50:25 +0700

    feat: Add AccountService🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/bankingsample/service/AccountService.java
@@ -0,0 +1,56 @@
+package id.my.hendisantika.bankingsample.service;
+
+import id.my.hendisantika.bankingsample.model.Account;
+import id.my.hendisantika.bankingsample.repository.AccountRepository;
+import id.my.hendisantika.bankingsample.repository.TransactionRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : banking-sample
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 30/12/25
+ * Time: 07.49
+ * To change this template use File | Settings | File Templates.
+ */
+@Service
+public class AccountService {
+
+    private final AccountRepository accountRepository;
+    private final TransactionRepository transactionRepository;
+
+    public AccountService(AccountRepository accountRepository,
+                          TransactionRepository transactionRepository) {
+        this.accountRepository = accountRepository;
+        this.transactionRepository = transactionRepository;
+    }
+
+    public Account getAccount(String sortCode, String accountNumber) {
+        Optional<Account> account = accountRepository
+                .findBySortCodeAndAccountNumber(sortCode, accountNumber);
+
+        account.ifPresent(value ->
+                value.setTransactions(transactionRepository
+                        .findBySourceAccountIdOrderByInitiationDate(value.getId())));
+
+        return account.orElse(null);
+    }
+
+    public Account getAccount(String accountNumber) {
+        Optional<Account> account = accountRepository
+                .findByAccountNumber(accountNumber);
+
+        return account.orElse(null);
+    }
+
+    public Account createAccount(String bankName, String ownerName) {
+        CodeGenerator codeGenerator = new CodeGenerator();
+        Account newAccount = new Account(bankName, ownerName, codeGenerator.generateSortCode(), codeGenerator.generateAccountNumber(), 0.00);
+        return accountRepository.save(newAccount);
+    }
+}
