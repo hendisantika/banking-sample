6762216714bdd6fa1922b041f1f1f44008e219ec
hendisantika <hendisantika@yahoo.co.id>
2025-12-30 07:56:59 +0700

    feat: Add handleValidationExceptions inb AccountRestController🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/bankingsample/controller/AccountRestController.java
+++ src/main/java/id/my/hendisantika/bankingsample/controller/AccountRestController.java
@@ -12,7 +12,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by IntelliJ IDEA.
@@ -80,5 +85,20 @@ public class AccountRestController {
         } else {
             return new ResponseEntity<>(constants.INVALID_SEARCH_CRITERIA, HttpStatus.BAD_REQUEST);
         }
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleValidationExceptions(
+            MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+
+        return errors;
     }
 }
