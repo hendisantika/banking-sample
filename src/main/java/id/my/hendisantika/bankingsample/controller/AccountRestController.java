ae881dcd7b65db9a92d3310958624c26eac483ac
hendisantika <hendisantika@yahoo.co.id>
2025-12-30 07:56:17 +0700

    feat: Add checkAccountBalance inb AccountRestController🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/bankingsample/controller/AccountRestController.java
+++ src/main/java/id/my/hendisantika/bankingsample/controller/AccountRestController.java
@@ -1,8 +1,18 @@
 package id.my.hendisantika.bankingsample.controller;
 
+import id.my.hendisantika.bankingsample.constants.constants;
+import id.my.hendisantika.bankingsample.model.Account;
 import id.my.hendisantika.bankingsample.service.AccountService;
+import id.my.hendisantika.bankingsample.util.AccountInput;
+import id.my.hendisantika.bankingsample.util.InputValidator;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,4 +35,28 @@ public class AccountRestController {
 
     private final AccountService accountService;
 
+    @PostMapping(value = "/accounts",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> checkAccountBalance(
+            // TODO In the future support searching by card number in addition to sort code and account number
+            @Valid @RequestBody AccountInput accountInput) {
+        log.debug("Triggered AccountRestController.accountInput");
+
+        // Validate input
+        if (InputValidator.isSearchCriteriaValid(accountInput)) {
+            // Attempt to retrieve the account information
+            Account account = accountService.getAccount(
+                    accountInput.getSortCode(), accountInput.getAccountNumber());
+
+            // Return the account details, or warn that no account was found for given input
+            if (account == null) {
+                return new ResponseEntity<>(constants.NO_ACCOUNT_FOUND, HttpStatus.OK);
+            } else {
+                return new ResponseEntity<>(account, HttpStatus.OK);
+            }
+        } else {
+            return new ResponseEntity<>(constants.INVALID_SEARCH_CRITERIA, HttpStatus.BAD_REQUEST);
+        }
+    }
 }
