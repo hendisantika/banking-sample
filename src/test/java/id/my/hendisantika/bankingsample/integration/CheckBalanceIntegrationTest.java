933a5de55df461dd5aeece4438cbe68f8ab77dbb
hendisantika <hendisantika@yahoo.co.id>
2025-12-30 18:56:48 +0700

    test: Add CheckBalanceIntegrationTest🫸🌀✏️📗🐧🐳

---
+++ src/test/java/id/my/hendisantika/bankingsample/integration/CheckBalanceIntegrationTest.java
@@ -0,0 +1,48 @@
+package id.my.hendisantika.bankingsample.integration;
+
+import id.my.hendisantika.bankingsample.controller.AccountRestController;
+import id.my.hendisantika.bankingsample.model.Account;
+import id.my.hendisantika.bankingsample.util.AccountInput;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : banking-sample
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 30/12/25
+ * Time: 18.56
+ * To change this template use File | Settings | File Templates.
+ */
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles(value = "local")
+class CheckBalanceIntegrationTest {
+
+    @Autowired
+    private AccountRestController accountRestController;
+
+    @Test
+    void givenAccountDetails_whenCheckingBalance_thenVerifyAccountCorrect() {
+        // given
+        var input = new AccountInput();
+        input.setSortCode("53-68-92");
+        input.setAccountNumber("73084635");
+
+        // when
+        var body = accountRestController.checkAccountBalance(input).getBody();
+
+        // then
+        var account = (Account) body;
+        assertThat(account).isNotNull();
+        assertThat(account.getOwnerName()).isEqualTo("Paul Dragoslav");
+        assertThat(account.getSortCode()).isEqualTo("53-68-92");
+        assertThat(account.getAccountNumber()).isEqualTo("73084635");
+    }
+}
