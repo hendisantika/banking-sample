817aeac4a07878e5152c490f67fc238a3bd30e12
hendisantika <hendisantika@yahoo.co.id>
2025-12-30 07:55:39 +0700

    feat: Add AccountRestController🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/bankingsample/controller/AccountRestController.java
@@ -0,0 +1,28 @@
+package id.my.hendisantika.bankingsample.controller;
+
+import id.my.hendisantika.bankingsample.service.AccountService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : banking-sample
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 30/12/25
+ * Time: 07.54
+ * To change this template use File | Settings | File Templates.
+ */
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("api/v1")
+public class AccountRestController {
+
+    private final AccountService accountService;
+
+}
