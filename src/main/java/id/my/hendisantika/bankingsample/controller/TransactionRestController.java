16f91771fe14166fe845e9c968d3f436e5c89707
hendisantika <hendisantika@yahoo.co.id>
2025-12-30 07:59:18 +0700

    feat: Add handleValidationExceptions in TransactionRestController🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/bankingsample/controller/TransactionRestController.java
+++ src/main/java/id/my/hendisantika/bankingsample/controller/TransactionRestController.java
@@ -14,10 +14,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static id.my.hendisantika.bankingsample.constants.constants.*;
 
@@ -105,5 +107,20 @@ public class TransactionRestController {
         } else {
             return new ResponseEntity<>(INVALID_SEARCH_CRITERIA, HttpStatus.BAD_REQUEST);
         }
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleValidationExceptions(
+            MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+
+        return errors;
     }
 }
